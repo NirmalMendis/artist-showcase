a51c94ace28fefa31ab571523d80d13bc0475f4d
NirmalMendis <nirmal.gmw@gmail.com>
2025-11-12 21:48:14 +0530

    refactor: fix limt and formatter issues

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { ChakraProvider } from '@chakra-ui/react';
 import systemTheme from 'theme/theme';
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
